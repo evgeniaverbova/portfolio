9e801dbe0a47e852b141428be2bb63ce2ae0aac9
yevgenia <evgeniaverbova25@gmail.com>
2022-11-22 10:51:56 +0200

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,10 @@
+"use strict"
+// let trueOrFalse = 58 < 18; 
+// console.log(trueOrFalse);
+let doYouLoveMe = false;
+if (doYouLoveMe) {
+    console.log(':)');
+} else{
+    console.log(':(');
+}
+
